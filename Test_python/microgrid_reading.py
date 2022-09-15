87e4e72b22ddb4831cee31510338a72caaa9208d
Francisco Viramontes <fviramontes8@unm.edu>
2022-09-15 15:45:34 -0600

    Update microgrid_reading
    Cleaned code to make reading it easier

--- Test_python/microgrid_reading.py
+++ Test_python/microgrid_reading.py
@@ -15,7 +15,6 @@ import numpy as np
 import sys
 
 def computing_fin_prob(l1, l11, relay):
-        
         pfc_C1 = svC1.svm_test_main(l1, relay)
         pfc_C2 = svC2.svm_test_main(l1, relay)
         pfc_C3 = svC3.svm_test_main(l1, relay)
@@ -27,12 +26,16 @@ def computing_fin_prob(l1, l11, relay):
         pCNN = cn.CNN_TEST1(np.array(l11), relay)
         
         # computing the marginal probabilities for fault detector and fault classifier
-        PFC1 = pfc_C1[0]*pCNN[0] + pfc_C2[0]*pCNN[1] + pfc_C3[0]*pCNN[2] + pfc_C4[0]*pCNN[3]
-        PFC2 = pfc_C1[1]*pCNN[0] + pfc_C2[1]*pCNN[1] + pfc_C3[1]*pCNN[2] + pfc_C4[1]*pCNN[3]
-        PFC3 = pfc_C1[2]*pCNN[0] + pfc_C2[2]*pCNN[1] + pfc_C3[2]*pCNN[2] + pfc_C4[2]*pCNN[3]
+        PFC1 = pfc_C1[0]*pCNN[0] + pfc_C2[0]*pCNN[1] + pfc_C3[0]*pCNN[2] 
+                + pfc_C4[0]*pCNN[3]
+        PFC2 = pfc_C1[1]*pCNN[0] + pfc_C2[1]*pCNN[1] + pfc_C3[1]*pCNN[2] 
+                + pfc_C4[1]*pCNN[3]
+        PFC3 = pfc_C1[2]*pCNN[0] + pfc_C2[2]*pCNN[1] + pfc_C3[2]*pCNN[2] 
+                + pfc_C4[2]*pCNN[3]
         PFC = [PFC1, PFC2, PFC3]
 
-        PF = pGP_C1*pCNN[0] + pGP_C2*pCNN[1] + pGP_C3*pCNN[2] + pGP_C4*pCNN[3]
+        PF = pGP_C1*pCNN[0] + pGP_C2*pCNN[1] + pGP_C3*pCNN[2] 
+                + pGP_C4*pCNN[3]
 
         return pCNN, PF, PFC
 
@@ -50,11 +53,37 @@ while(client.connect()):
         try:
 
                 result = client.read_holding_registers(0,26,unit =1) 
-                decoder = BinaryPayloadDecoder.fromRegisters(result.registers, byteorder=Endian.Little, wordorder=Endian.Little)
+                decoder = BinaryPayloadDecoder.fromRegisters(
+                    result.registers, 
+                    byteorder=Endian.Little, 
+                    wordorder=Endian.Little
+                )
 	
-                array = [decoder.decode_32bit_float(),decoder.decode_32bit_float(),decoder.decode_32bit_float(),decoder.decode_32bit_float(),decoder.decode_32bit_float(),decoder.decode_32bit_float(),decoder.decode_32bit_float(),decoder.decode_32bit_float(),decoder.decode_32bit_float(),decoder.decode_32bit_float(),decoder.decode_32bit_float(),decoder.decode_32bit_float(),decoder.decode_32bit_float()]
+                array = [
+                        decoder.decode_32bit_float(),
+                        decoder.decode_32bit_float(),
+                        decoder.decode_32bit_float(),
+                        decoder.decode_32bit_float(),
+                        decoder.decode_32bit_float(),
+                        decoder.decode_32bit_float(),
+                        decoder.decode_32bit_float(),
+                        decoder.decode_32bit_float(),
+                        decoder.decode_32bit_float(),
+                        decoder.decode_32bit_float(),
+                        decoder.decode_32bit_float(),
+                        decoder.decode_32bit_float(),
+                        decoder.decode_32bit_float()
+                ]
                 
-                l1 = [array[0], array[1], array[2],array[3], array[4], array[5]]     #voltage and current values saved here
+                #voltage and current values saved here
+                l1 = [
+                        array[0], 
+                        array[1], 
+                        array[2],
+                        array[3], 
+                        array[4], 
+                        array[5]
+                ]     
                 relay = 'RTL3'
                 l11.append(l1)
                 print("\n")
