2ecff2558ae3a81afbc74af6222cd6eb24c48793
Francisco Viramontes <fviramontes8@unm.edu>
2022-09-27 23:36:03 -0600

    Update microgrid_reading
    Moved modbus code to its own function

--- Test_python/microgrid_reading.py
+++ Test_python/microgrid_reading.py
@@ -26,77 +26,77 @@ def computing_fin_prob(l1, l11, relay):
         pCNN = cn.CNN_TEST1(np.array(l11), relay)
         
         # computing the marginal probabilities for fault detector and fault classifier
-        PFC1 = pfc_C1[0]*pCNN[0] + pfc_C2[0]*pCNN[1] + pfc_C3[0]*pCNN[2] 
-                + pfc_C4[0]*pCNN[3]
-        PFC2 = pfc_C1[1]*pCNN[0] + pfc_C2[1]*pCNN[1] + pfc_C3[1]*pCNN[2] 
-                + pfc_C4[1]*pCNN[3]
-        PFC3 = pfc_C1[2]*pCNN[0] + pfc_C2[2]*pCNN[1] + pfc_C3[2]*pCNN[2] 
-                + pfc_C4[2]*pCNN[3]
+        PFC1 = pfc_C1[0]*pCNN[0] + pfc_C2[0]*pCNN[1] + pfc_C3[0]*pCNN[2] + pfc_C4[0]*pCNN[3]
+        PFC2 = pfc_C1[1]*pCNN[0] + pfc_C2[1]*pCNN[1] + pfc_C3[1]*pCNN[2] + pfc_C4[1]*pCNN[3]
+        PFC3 = pfc_C1[2]*pCNN[0] + pfc_C2[2]*pCNN[1] + pfc_C3[2]*pCNN[2] + pfc_C4[2]*pCNN[3]
         PFC = [PFC1, PFC2, PFC3]
 
-        PF = pGP_C1*pCNN[0] + pGP_C2*pCNN[1] + pGP_C3*pCNN[2] 
-                + pGP_C4*pCNN[3]
+        PF = pGP_C1*pCNN[0] + pGP_C2*pCNN[1] + pGP_C3*pCNN[2] + pGP_C4*pCNN[3]
 
         return pCNN, PF, PFC
 
 
 #[V1, V2, V0, I1, I2, I0, fault status_boolean (0 or 1), fault type (2 fro AB as Adams doc), fault_resistance (0.5), RTL1, RTL2, RTL3, RTL4]
 
-# modbus
-client = ModbusTcpClient('192.168.1.19', port=502)
-print(client.connect())
-print(client.is_socket_open())
-l11 = []
-i=0
-while(client.connect()):  
-        time.sleep(1)
-        try:
+def begin_modbus():
+        # modbus
+        client = ModbusTcpClient('192.168.1.19', port=502)
+        print(client.connect())
+        print(client.is_socket_open())
+        l11 = []
+        i=0
+        while(client.connect()):  
+                time.sleep(1)
+                try:
+                        result = client.read_holding_registers(0,26,unit =1) 
+                        decoder = BinaryPayloadDecoder.fromRegisters(
+                        result.registers, 
+                        byteorder=Endian.Little, 
+                        wordorder=Endian.Little
+                        )
+	    
+                        array = [
+                                decoder.decode_32bit_float(),
+                                decoder.decode_32bit_float(),
+                                decoder.decode_32bit_float(),
+                                decoder.decode_32bit_float(),
+                                decoder.decode_32bit_float(),
+                                decoder.decode_32bit_float(),
+                                decoder.decode_32bit_float(),
+                                decoder.decode_32bit_float(),
+                                decoder.decode_32bit_float(),
+                                decoder.decode_32bit_float(),
+                                decoder.decode_32bit_float(),
+                                decoder.decode_32bit_float(),
+                                decoder.decode_32bit_float()
+                        ]
+                        
+                        #voltage and current values saved here
+                        l1 = [
+                                array[0], 
+                                array[1], 
+                                array[2],
+                                array[3], 
+                                array[4], 
+                                array[5]
+                        ]     
+                        relay = 'RTL3'
+                        l11.append(l1)
+                        print("\n")
+                        i = i+1
+                        print('iteration completed = ', i)
+                        print(array)
+                        [pCNN, PF, PFC] = computing_fin_prob(l1, l11, relay)
+                        print('Configuration classifier (1DCNN): ',pCNN)
+                        print('Fault Detector (GP+OCSVM): ',PF)
+                        print('Fault classifier (SVM): ',PFC)
+	            # result1 = client.read_holding_registers()
+                except KeyboardInterrupt:
+                        print('Keyboard Interrupt')
+                        client.close()
 
-                result = client.read_holding_registers(0,26,unit =1) 
-                decoder = BinaryPayloadDecoder.fromRegisters(
-                    result.registers, 
-                    byteorder=Endian.Little, 
-                    wordorder=Endian.Little
-                )
-	
-                array = [
-                        decoder.decode_32bit_float(),
-                        decoder.decode_32bit_float(),
-                        decoder.decode_32bit_float(),
-                        decoder.decode_32bit_float(),
-                        decoder.decode_32bit_float(),
-                        decoder.decode_32bit_float(),
-                        decoder.decode_32bit_float(),
-                        decoder.decode_32bit_float(),
-                        decoder.decode_32bit_float(),
-                        decoder.decode_32bit_float(),
-                        decoder.decode_32bit_float(),
-                        decoder.decode_32bit_float(),
-                        decoder.decode_32bit_float()
-                ]
-                
-                #voltage and current values saved here
-                l1 = [
-                        array[0], 
-                        array[1], 
-                        array[2],
-                        array[3], 
-                        array[4], 
-                        array[5]
-                ]     
-                relay = 'RTL3'
-                l11.append(l1)
-                print("\n")
-                i = i+1
-                print('iteration completed = ', i)
-                print(array)
-                [pCNN, PF, PFC] = computing_fin_prob(l1, l11, relay)
-                print('Configuration classifier (1DCNN): ',pCNN)
-                print('Fault Detector (GP+OCSVM): ',PF)
-                print('Fault classifier (SVM): ',PFC)
-	        # result1 = client.read_holding_registers()
-        except KeyboardInterrupt:
-                print('Keyboard Interrupt')
-                client.close()
+def main():
+        pass
 
-
+if __name__ == "__main__":
+        main()
