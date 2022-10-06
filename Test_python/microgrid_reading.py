586fb768ab227385cbdaefc3e7647573e4c41c81
Francisco Viramontes <fviramontes8@unm.edu>
2022-10-05 19:49:48 -0600

    Reformat Test_python/microgrid_reading
    Changed formatting to be compliant with PEP8

--- Test_python/microgrid_reading.py
+++ Test_python/microgrid_reading.py
@@ -16,6 +16,7 @@ import sys
 
 import scipy.io as scio
 
+
 def computing_fin_prob(l1, l11, relay):
     pfc_C1 = svC1.svm_test_main(l1, relay)
     pfc_C2 = svC2.svm_test_main(l1, relay)
@@ -27,9 +28,12 @@ def computing_fin_prob(l1, l11, relay):
     pGP_C4 = GPC4.GP_test_main(l1, relay)
     pCNN = cn.CNN_TEST1(np.array(l11), relay)
     # computing the marginal probabilities for fault detector and fault classifier
-    PFC1 = pfc_C1[0]*pCNN[0] + pfc_C2[0]*pCNN[1] + pfc_C3[0]*pCNN[2] + pfc_C4[0]*pCNN[3]
-    PFC2 = pfc_C1[1]*pCNN[0] + pfc_C2[1]*pCNN[1] + pfc_C3[1]*pCNN[2] + pfc_C4[1]*pCNN[3]
-    PFC3 = pfc_C1[2]*pCNN[0] + pfc_C2[2]*pCNN[1] + pfc_C3[2]*pCNN[2] + pfc_C4[2]*pCNN[3]
+    PFC1 = pfc_C1[0]*pCNN[0] + pfc_C2[0]*pCNN[1]\
+        + pfc_C3[0]*pCNN[2] + pfc_C4[0]*pCNN[3]
+    PFC2 = pfc_C1[1]*pCNN[0] + pfc_C2[1]*pCNN[1]\
+        + pfc_C3[1]*pCNN[2] + pfc_C4[1]*pCNN[3]
+    PFC3 = pfc_C1[2]*pCNN[0] + pfc_C2[2]*pCNN[1]\
+        + pfc_C3[2]*pCNN[2] + pfc_C4[2]*pCNN[3]
     PFC = [PFC1, PFC2, PFC3]
     PF = pGP_C1*pCNN[0] + pGP_C2*pCNN[1] + pGP_C3*pCNN[2] + pGP_C4*pCNN[3]
 
@@ -85,19 +89,21 @@ def begin_modbus():
             i = i+1
             print('iteration completed = ', i)
             print(decoded_features)
-            [pCNN, PF, PFC] = computing_fin_prob(l1, l11, relay)
-            print('Configuration classifier (1DCNN): ',pCNN)
-            print('Fault Detector (GP+OCSVM): ',PF)
-            print('Fault classifier (SVM): ',PFC)
+            [p_cnn, pf, pfc] = computing_fin_prob(l1, l11, relay)
+            print(f"Configuration classifier (1DCNN): {p_cnn}")
+            print(f"Fault Detector (GP+OCSVM): {pf}")
+            print(f"Fault classifier (SVM): {pfc}")
             # result1 = client.read_holding_registers()
         except KeyboardInterrupt:
             print('Keyboard Interrupt')
             client.close()
 
+
 def main():
     mat_file = scio.loadmat("../Data/sample_C3RTL3.mat")
     print(f"Relay keys:  {mat_file.keys()}")
     print(f"Data: {mat_file['sample_C3RTL3'].shape}")
+
 
 if __name__ == "__main__":
     main()
