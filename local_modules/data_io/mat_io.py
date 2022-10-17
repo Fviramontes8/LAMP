f1d1415940c034c234bcbf662da70c02710ab5a0
Francisco Viramontes <fviramontes8@unm.edu>
2022-10-17 11:12:53 -0600

    Update data_io/mat_io
    Updated documentation block so specify the return type to expect and
    reformatted the brief portion

--- local_modules/data_io/mat_io.py
+++ local_modules/data_io/mat_io.py
@@ -1,17 +1,18 @@
 import scipy.io as scio
+
 """
-A helper function to read the .mat files in the Data/ folder.
-Typically when the file is read, it is in the form of a dictionary with the
-following keys:
-    ['__header__', '__version__', '__globals__', 'sample_CXRTL3']
-The key 'sample_CXRTL3' contains the interesting data. The 'CX' part
-describes the configuration. The X can be 1, 2, 3, or 4. The 'RTL3" means
-that the data is coming from relay 3.
+@brief A helper function to read the .mat files in the Data/ folder.
+    Typically when the file is read, it is in the form of a dictionary 
+    with the following keys:
+        ['__header__', '__version__', '__globals__', 'sample_CXRTL3']
+    The key 'sample_CXRTL3' contains the interesting data. The 'CX' part
+    describes the configuration. The X can be 1, 2, 3, or 4. The 'RTL3" means
+    that the data is coming from relay 3.
 
-Keyword arguments:
-path -- A string describing the absolute or relative path to the .mat file.
+@param path -- A string describing the absolute or relative path to 
+    the .mat file.
 
-Returns ???
+@returns A dictionary containing the contents of the .mat file.
 """
 def open_mat_file(path: str) -> dict:
     return scio.loadmat(path);
