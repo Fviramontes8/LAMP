308f6edd1e81987149a3d01d98f2bc70d91c1a1e
Francisco Viramontes <fviramontes8@unm.edu>
2022-10-17 11:14:50 -0600

    Update plot_utilities/plot_utils
    Changed documentation block to be compatible with doxygen.
    
    Added save_subplots function that is similar to create_subplots but
    rather than show the user the subplots, it will save it as a png.

--- local_modules/plot_utilities/plot_utils.py
+++ local_modules/plot_utilities/plot_utils.py
@@ -2,13 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 """
-Supporting function to help visualize the relay data
+@brief Supporting function to help visualize the relay data
 
-Keyword arguments:
-data -- Source data to plot
-labels -- Labels to describe data, must be the same size as data
-dim0 -- Number of rows for the subplots
-dim1 -- Number of columns for the subplots
+@param data -- Source data to plot
+@param labels -- Labels to describe data, must be the same size as data
+@param dim0 -- Number of rows for the subplots
+@param dim1 -- Number of columns for the subplots
 """
 def create_subplots(data: np.array, labels: list, 
         dim0: int, dim1: int) -> None:
@@ -24,4 +23,30 @@ def create_subplots(data: np.array, labels: list,
         ax[ax_row][ax_col].set_title(labels[i])
     plt.show()
 
+"""
+@brief Same functionality as create_subplots but rather than opening a 
+    window to show the data, it will save the figure as a png
+
+@param data -- Source data to plot
+@param labels -- Labels to describe data, must be the same size as data
+@param png_filename -- Filename to save the figure as a png
+@param dim0 -- Number of rows for the subplots
+@param dim1 -- Number of columns for the subplots
+"""
+def save_subplots(data: np.array, labels: list, png_filename: str,
+        dim0: int, dim1: int) -> None:
+    assert(len(data) == len(labels))
+
+    x_data = [ 
+        i for i in range(len(data[0])) 
+    ]
+    fig, ax = plt.subplots(dim0, dim1, sharex=True);
+    fig.suptitle("Solar relay timeseries of 200 samples")
+
+    for i in range(len(data)):
+        ax_row = i // ax.shape[1]
+        ax_col = i % ax.shape[1]
+        ax[ax_row][ax_col].plot(x_data, data[i])
+        ax[ax_row][ax_col].set_title(labels[i])
+    plt.savefig(png_filename)
 
