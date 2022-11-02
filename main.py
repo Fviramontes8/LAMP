716b3c6907b20c5d7b092124b0c958adb74f8dd5
Francisco Viramontes <fviramontes8@unm.edu>
2022-11-01 21:26:56 -0600

    Update main
    Add driving code to simulate an experiment which holds on to 100 values
    and does inference on 100 additional values

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
+import local_modules.plot_utilities.plot_utils as pu
 from local_modules.data_io.mat_io import open_mat_file
-from local_modules.plot_utilities.plot_utils import create_subplots
 from local_modules.model_inference import fault_detector
 
 from model_inference.microgrid_reading import compute_fault_probabilities
-
 
 def main() -> None:
     relay_data_dict = open_mat_file("Data/sample_C1RTL3.mat")
@@ -29,21 +29,32 @@ def main() -> None:
 
     relay_data_array = np.array(relay_data_dict[data_key]).T
     relay_data_length = len(relay_data_array[0])
-    # create_subplots(relay_data_array, feature_labels, 3, 4)
+    # pu.create_subplots(relay_data_array, feature_labels, 3, 4)
 
-    testing_data_slice = []
-    total_testing_data = []
-    for i in range(relay_data_length):
-        testing_data_slice = np.atleast_2d(relay_data_array[1:7, i])
+    p_cnn_history = np.zeros((1, 4))
+    pf_history = []
+    pfc_history = np.zeros((1, 3))
+
+    # CNN component requires 101 values before it can begin inferencing
+    #   on new data
+    total_testing_data = [ relay_data_array[1:7, i] for i in range(101) ]
+    for i in range(101, relay_data_length):
+        testing_data_slice = list(relay_data_array[1:7, i])
         total_testing_data.append(testing_data_slice)
-        print(f"Data slice shape: {testing_data_slice.shape}")
-        print(f"total testing data shape: {len(total_testing_data)}")
         p_cnn, pf, pfc = fault_detector.compute_fault_probabilities(
             testing_data_slice,
             total_testing_data,
             relay_name
         )
-        print(f"p_cnn: {p_cnn}, pf: {pf}, pfc: {pfc}")
+        #print(f"Iteration {i-101}, p_cnn: {p_cnn}, pf: {pf}, pfc: {pfc}")
+
+        p_cnn_history = np.vstack((p_cnn_history, p_cnn))
+        pf_history.append(pf)
+        pfc_history = np.vstack((pfc_history, pfc))
+        pu.save_cnn_history(p_cnn_history)
+        pu.save_pf_history(pf_history)
+        pu.save_pfc_history(pfc_history)
+        time.sleep(0.5)
 
 
 if __name__ == "__main__":
