c374e548263f90ef8feb61d857a1f3dac3077b3a
Francisco Viramontes <fviramontes8@unm.edu>
2022-12-14 01:06:39 -0700

    Update main
    Added modbus code so that inference can occur with modbus as the data source

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import numpy as np
 import time
 
+from pymodbus.client.sync import ModbusTcpClient
+from pymodbus.constants import Endian
+from pymodbus.payload import BinaryPayloadDecoder
+
 import local_modules.plot_utilities.plot_utils as pu
-from local_modules.data_io.mat_io import open_mat_file
 from local_modules.model_inference import fault_detector
 
 
 def main() -> None:
-    # relay_data_dict = open_mat_file("Data/RTL1_C1_OPAL.mat")
-    rtl_c1 = open_mat_file("Data/RTL1_C1_OPAL.mat")
-    rtl_c2 = open_mat_file("Data/RTL1_C2_OPAL.mat")
-    rtl_c3 = open_mat_file("Data/RTL1_C3_OPAL.mat")
-    rtl_c4 = open_mat_file("Data/RTL1_C4_OPAL.mat")
+    ip_address = "192.168.1.19"
+    port = 502
+    client = ModbusTcpClient(ip_address, port)
+    print(client.connect())
+    print(client.is_socket_open())
+    total_data = []
+    iteration = 0
 
-    data_key = "all_quantities"
-    relay_name = "RTL3"
-    feature_labels = [
+    decoded_feature_labels = [
         "Timestamp",
         "V1",
         "V2",
@@ -25,58 +28,62 @@ def main() -> None:
         "I0",
         "Configuration",
         "Fault Status",
-        "Fault Type (1 - 3)",
+        "Fault Type (1 - 4)",
         "Fault Location (Fault bus 20)",
     ]
-
-    slice_start = 0
-    slice_end = 500
-    relay_data_array = np.array(rtl_c1[data_key].T[slice_start:slice_end])
-    jump = 500
-    for i in range(7):
-        np.append(relay_data_array, rtl_c2[data_key].T[slice_start:slice_end])
-        np.append(relay_data_array, rtl_c3[data_key].T[slice_start:slice_end])
-        np.append(relay_data_array, rtl_c4[data_key].T[slice_start:slice_end])
-        slice_start += jump
-        slice_end += jump
-        if i == 6:
-            continue
-        np.append(relay_data_array, rtl_c1[data_key].T[slice_start:slice_end])
-
-    # relay_data_array = np.array(relay_data_dict[data_key]).T
-    relay_data_length = len(relay_data_array[0])
-    start = 102
-#    pu.create_subplots(
-#        relay_data_array[:10, start:slice_len],
-#        feature_labels[:10],
-#        2,
-#        5
-#    )
 
     p_cnn_history = np.zeros((1, 4))
     pf_history = []
     pfc_history = np.zeros((1, 3))
 
-    # CNN component requires 101 values before it can begin inferencing
-    #   on new data
-    total_testing_data = [relay_data_array[1:7, i] for i in range(101)]
-    for i in range(start, relay_data_length):  # relay_data_length):
-        testing_data_slice = list(relay_data_array[1:7, i])
-        total_testing_data.append(testing_data_slice)
-        p_cnn, pf, pfc = fault_detector.compute_fault_probabilities(
-            testing_data_slice,
-            total_testing_data,
-            relay_name
-        )
-        print(f"Iteration {i-start}, p_cnn: {p_cnn}, pf: {pf}, pfc: {pfc}")
+    while client.connect():
+        time.sleep(1)
+        try:
+            result = client.read_holding_registers(0, 26, unit=1)
+            decoder = BinaryPayloadDecoder.fromRegisters(
+                result.registers,
+                byteorder=Endian.Little,
+                wordorder=Endian.Little
+            )
 
-        p_cnn_history = np.vstack((p_cnn_history, p_cnn))
-        pf_history.append(pf[0])
-        pfc_history = np.vstack((pfc_history, pfc))
-        pu.save_cnn_history(p_cnn_history)
-        pu.save_pf_history(pf_history)
-        pu.save_pfc_history(pfc_history)
-        time.sleep(0.1)
+            decoded_features = [
+                decoder.decode_32bit_float(),
+                decoder.decode_32bit_float(),
+                decoder.decode_32bit_float(),
+                decoder.decode_32bit_float(),
+                decoder.decode_32bit_float(),
+                decoder.decode_32bit_float(),
+                decoder.decode_32bit_float(),
+                decoder.decode_32bit_float(),
+                decoder.decode_32bit_float(),
+                decoder.decode_32bit_float(),
+                decoder.decode_32bit_float(),
+                decoder.decode_32bit_float(),
+                decoder.decode_32bit_float()
+            ]
+
+            # L1 contains [ V1, V2, V0, I1, I2, I0 ]
+            # Voltage and current values
+            l1 = decoded_features[:6]
+
+            relay = 'RTL3'
+            total_data.append(l1)
+            p_cnn, pf, pfc = fault_detector.compute_fault_probabilities(l1, total_data, relay)
+
+            print(f"Iteration: {iteration} completed\n")
+            # print(f"Iteration {iteration}, p_cnn: {p_cnn}, pf: {pf}, pfc: {pfc}")
+
+            p_cnn_history = np.vstack((p_cnn_history, p_cnn))
+            pf_history.append(pf[0])
+            pfc_history = np.vstack((pfc_history, pfc))
+            pu.save_cnn_history(p_cnn_history)
+            pu.save_pf_history(pf_history)
+            pu.save_pfc_history(pfc_history)
+
+            iteration = iteration + 1
+        except KeyboardInterrupt:
+            print('Keyboard Interrupt')
+            client.close()
 
 
 if __name__ == "__main__":
